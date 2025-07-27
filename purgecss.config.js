c49a84392cbc298a4563c860c9a5504932b6c1bc
Devid Melnik <devid.melnik@hotmail.com>
2025-07-27 13:19:18 +0300

    финалка и дист вместе

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,12 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.js'], // пути к твоим HTML/JS
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  content: ['index.html', 'js/**/*.js'],
-  css: ['css/styles.css'],
+  plugins: [
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
